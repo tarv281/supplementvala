76ccd745b6dc15e35c59f58341d66065d3d1a606
tarv281 <39852294+tarv281@users.noreply.github.com>
2019-11-13 13:12:31 +0530

    m

--- admin/index.php
+++ admin/index.php
@@ -1,49 +1,19 @@
-<?php 
-session_start();  
-if (!isset($_SESSION['admin_id'])) {
-  header("location:login.php");
+<?php
+// Version
+define('VERSION', '3.0.3.2');
+
+// Configuration
+if (is_file('config.php')) {
+	require_once('config.php');
 }
 
-include "./templates/top.php"; 
+// Install
+if (!defined('DIR_APPLICATION')) {
+	header('Location: ../install/index.php');
+	exit;
+}
 
-?>
- 
-<?php include "./templates/navbar.php"; ?>
+// Startup
+require_once(DIR_SYSTEM . 'startup.php');
 
-<div class="container-fluid">
-  <div class="row">
-    
-    <?php include "./templates/sidebar.php"; ?>
-
-      <!-- <canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas> -->
-
-      <h2>Other Admins</h2>
-      <div class="table-responsive">
-        <table class="table table-striped table-sm">
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Status</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody id="admin_list">
-            <tr>
-              <td>1,001</td>
-              <td>Lorem</td>
-              <td>ipsum</td>
-              <td>dolor</td>
-              <td>sit</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </main>
-  </div>
-</div>
-
-<?php include "./templates/footer.php"; ?>
-
-<script type="text/javascript" src="./js/admin.js"></script>
+start('admin');
